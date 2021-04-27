6015e60f3a5bd64e8f42ae9ec834e91412cb5190
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-04-26 21:39:27 -0400

    built item update action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ItemsController < ApplicationController
 
     def index
@@ -8,6 +10,21 @@ class ItemsController < ApplicationController
     def show
         item = Item.find(params[:id])
         render json: ItemSerializer.new(item, {include: [:category]})
+    end
+
+    def update
+        item = Item.find(params[:id])
+        if item.update(item_params)
+            render json: ItemSerializer.new(item)
+        else
+            render json: {error: "Unable to save changes"}
+        end
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:name, :price, :description, :quantity, :image_url, :category_id)
     end
 
 end
